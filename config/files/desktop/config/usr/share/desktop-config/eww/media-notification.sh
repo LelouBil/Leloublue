01412e378db6ab7db99a557d039b48d3696d5783
LelouBil <bilel.medimegh@gmail.com>
2024-03-27 16:25:10 +0100

    media notifications

--- config/files/desktop/config/usr/share/desktop-config/eww/media-notification.sh
+++ config/files/desktop/config/usr/share/desktop-config/eww/media-notification.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 set -euxo pipefail
 
-pid_file="/tmp/media-notification.pid"
-
-if [[ -f $pid_file ]]; then
+close_file="/tmp/media-notification.time"
+cur_time_seconds=$(date +%s)
+next_close_time=$((cur_time_seconds + 3))
+if [[ -f $close_file ]]; then
   echo "already exists"
-  other_pid=$(cat $pid_file)
-  kill $other_pid
+  echo $next_close_time > $close_file
+  exit
 fi
+echo $next_close_time > $close_file
 
-echo $$ > $pid_file
-
+eww update time-visible=true -c "$EWW_CONFIG_DIR"
 eww open media-notification -c "$EWW_CONFIG_DIR"
-sleep 3
-eww close media-notification -c "$EWW_CONFIG_DIR"
+should_close=false
 
-rm $pid_file
+while [[ ! $should_close = true ]]; do
+
+sleep 1
+cur_time_seconds=$(date +%s)
+close_time=$(cat $close_file)
+if [[ $cur_time_seconds -gt $close_time ]]; then
+  should_close=true
+  else
+    should_close=false
+    fi
+done
+eww close media-notification -c "$EWW_CONFIG_DIR"
+eww update time-visible=false -c "$EWW_CONFIG_DIR"
+rm $close_file
+
+
