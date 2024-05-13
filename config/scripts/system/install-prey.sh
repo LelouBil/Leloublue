3151ed1f65db62b92651cb16880c2ba146960fed
LelouBil <bilel.medimegh@gmail.com>
2024-05-13 15:41:03 +0200

    fix prey user

--- config/scripts/system/install-prey.sh
+++ config/scripts/system/install-prey.sh
@@ -4,5 +4,4 @@ set -euxo pipefail
 git clone https://github.com/prey/prey-node-client /usr/share/prey-agent
 cd /usr/share/prey-agent
 npm ci
-groupadd --system prey
 useradd --system --no-create-home --group prey --home-dir /usr/share/prey-agent --shell /bin/false prey
