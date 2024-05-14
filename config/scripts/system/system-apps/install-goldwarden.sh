62fcef08588aa6535b8327f48f1a2890f955d012
LelouBil <bilel.medimegh@gmail.com>
2024-05-14 15:59:06 +0200

    goldwarden fix

--- config/scripts/system/system-apps/install-goldwarden.sh
+++ config/scripts/system/system-apps/install-goldwarden.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 set -euxo pipefail
-RPM_URL=$(curl https://api.github.com/repos/quexten/goldwarden/releases/latest 2> /dev/null | grep -oE "https://(.*)\.rpm"https://github.com/quexten/goldwarden/releases/download/v0.3.2/goldwarden-0.3.2-1.el7.x86_64.rpm)
+RPM_URL=$(curl https://api.github.com/repos/quexten/goldwarden/releases/latest 2> /dev/null | grep -oE "https://(.*)\.rpm")
 curl -o /tmp/goldwarden.rpm -L $RPM_URL
 rpm-ostree install /tmp/goldwarden.rpm
 rm /tmp/goldwarden.rpm
