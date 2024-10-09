27e73ac8cfd2b79cf1cc19e0de2771313312b205
LelouBil <bilel.medimegh@gmail.com>
2024-10-09 12:05:17 +0200

    fix waybar

--- files/scripts/desktop/install-brillo.sh
+++ files/scripts/desktop/install-brillo.sh
@@ -3,4 +3,4 @@ set -euxo pipefail
 git clone https://gitlab.com/cameronnemo/brillo /tmp/brillo
 cd /tmp/brillo
 make
-make install install.polkit SYSCONFIGDIR=/usr/etc PREFIX=/usr
+make install install.polkit SYSCONFIGDIR=/etc PREFIX=/usr
